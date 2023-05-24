a44c19b6633226f119394edada667d9a0f8e0698
Fidele <fidele@tag-ip.com>
2023-05-24 19:39:56 +0300

    edit profil

---
+++ src/Components/PurpleButton.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {TouchableOpacity, Text, StyleSheet, Platform} from 'react-native';
+
+export default function PurpleButton({label, onPress}) {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.buttonText}>{label}</Text>
+    </TouchableOpacity>
+  );
+}
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 10,
+    height: 49,
+    maxWidth: 370,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9154FD',
+    elevation: 4,
+    shadowColor: 'rgba(145, 84, 253, 0.8)',
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.6,
+    shadowRadius: 14,
+  },
+  buttonText: {
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: 500,
+    fontSize: 17,
+    lineHeight: 21,
+    textAlign: 'center',
+    color: '#FFFFFF',
+  },
+});
